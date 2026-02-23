e973c56611f9d972f9371c60668296b646fbe06a
Kyrubi <u20221c486@upc.edu.pe>
2026-02-22 21:02:04 -0500

    feat: refactorizacion de Invoice para usar el InvoiceDocument y actualizaciond el metodo factory

--- LiquiLabs.Vankoo.Invoicing/Domain/Aggregates/Invoice.cs
+++ LiquiLabs.Vankoo.Invoicing/Domain/Aggregates/Invoice.cs
@@ -1,14 +1,16 @@
 using LiquiLabs.Vankoo.Invoicing.Domain.ValueObjects;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace LiquiLabs.Vankoo.Invoicing.Domain.Aggregates;
 
 public sealed class Invoice
 {
+    [BsonId]
     public InvoiceId Id { get; private set; }
     public MypeId MypeId { get; private set; }
     public PayerData? PayerData { get; private set; }
     public InvoiceStatus Status { get; private set; }
-    public DocumentUrl FileUrl { get; private set; }
+    public InvoiceDocument Document { get; private set; }
     public Money? TotalAmount { get; private set; }
     public string? OcrOperationId { get; private set; }
     public InvoiceMetadata? Metadata { get; private set; }
@@ -19,28 +21,24 @@ public sealed class Invoice
     public DateTime UpdatedAt { get; private set; }
 
     private readonly List<InvoiceLineItem> _items = [];
-    
 
     // ========== CONSTRUCTOR PRIVADO (para MongoDB) ==========
     private Invoice() { }
 
     // ========== FACTORY METHOD ==========
-    public static Invoice Create(MypeId mypeId, DocumentUrl fileUrl)
+    public static Invoice Create(MypeId mypeId, InvoiceDocument document)
     {
         ArgumentNullException.ThrowIfNull(mypeId);
-        ArgumentNullException.ThrowIfNull(fileUrl);
+        ArgumentNullException.ThrowIfNull(document);
 
-        var invoice = new Invoice
+        return new Invoice
         {
             Id = InvoiceId.NewId(),
             MypeId = mypeId,
-            FileUrl = fileUrl,
+            Document = document,
             Status = InvoiceStatus.UPLOADED,
             CreatedAt = DateTime.UtcNow,
             UpdatedAt = DateTime.UtcNow
         };
-
-        return invoice;
     }
-
 }
