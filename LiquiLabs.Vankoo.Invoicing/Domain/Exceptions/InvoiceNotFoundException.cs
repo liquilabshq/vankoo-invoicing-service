3ff5e5c276d1a70f98602107475386581ec63695
Kyrubi <u20221c486@upc.edu.pe>
2026-03-01 20:34:17 -0500

    feat: actuliza el InvoiceNotFoundException para extender de EntityNotFoundException y mejora el mensaje de error

--- LiquiLabs.Vankoo.Invoicing/Domain/Exceptions/InvoiceNotFoundException.cs
+++ LiquiLabs.Vankoo.Invoicing/Domain/Exceptions/InvoiceNotFoundException.cs
@@ -1,14 +1,13 @@
+using LiquiLabs.Vankoo.Invoicing.Shared.Domain.Exceptions;
+
 namespace LiquiLabs.Vankoo.Invoicing.Domain.Exceptions;
-/// <summary>
-/// Se lanza cuando no se encuentra una factura por su ID.
-/// Útil en queries.
-/// </summary>
-public class InvoiceNotFoundException : InvoiceDomainException
+
+public sealed class InvoiceNotFoundException : EntityNotFoundException
 {
     public string InvoiceId { get; }
 
     public InvoiceNotFoundException(string invoiceId)
-        : base($"Invoice with ID '{invoiceId}' was not found")
+        : base("INVOICE_NOT_FOUND", $"Invoice with ID '{invoiceId}' was not found")
     {
         InvoiceId = invoiceId;
     }
