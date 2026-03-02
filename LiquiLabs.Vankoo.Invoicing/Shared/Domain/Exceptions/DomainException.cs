e3f91155d4d3a426eddab0dccad97347106ac5be
Kyrubi <u20221c486@upc.edu.pe>
2026-03-01 20:41:24 -0500

    feat: agrega excepciones base para manejo de errores de dominio

---
+++ LiquiLabs.Vankoo.Invoicing/Shared/Domain/Exceptions/DomainException.cs
@@ -0,0 +1,18 @@
+namespace LiquiLabs.Vankoo.Invoicing.Shared.Domain.Exceptions;
+
+public abstract class DomainException : Exception
+{
+    public string ErrorCode { get; }
+
+    protected DomainException(string errorCode, string message)
+        : base(message)
+    {
+        ErrorCode = errorCode;
+    }
+
+    protected DomainException(string errorCode, string message, Exception innerException)
+        : base(message, innerException)
+    {
+        ErrorCode = errorCode;
+    }
+}
